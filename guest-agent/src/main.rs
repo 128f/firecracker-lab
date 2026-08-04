16dd29b3aa97db0e852b0ab3992ded715549377b
cburke <cburke91@pm.me>
2026-08-03 23:03:54 -0700

    Clean and consolidate
    Cleaned everything up into functions
    Also moved all the unwraps together so we can deal with them later

--- guest-agent/src/main.rs
+++ guest-agent/src/main.rs
@@ -2,21 +2,13 @@ use rustix::buffer::spare_capacity;
 use rustix::event::epoll;
 use rustix::fs::{Mode, mkdir};
 use rustix::fs::{OFlags, fcntl_getfl, fcntl_setfl};
-use rustix::io::Errno;
 use rustix::io::read;
+use rustix::io::{self, Errno};
 use rustix::mount::{MountFlags, mount};
+use rustix::termios::{OptionalActions, tcgetattr, tcsetattr};
 use std::io::{Write, stdin};
-use std::os::fd::{AsFd, FromRawFd, OwnedFd};
+use std::os::fd::{AsFd, BorrowedFd, FromRawFd, OwnedFd};
 use syscalls::{Sysno, syscall};
-
-fn early_mounts() {
-    // TODO: this is sloppy af and needs a bunch of safeties
-    // and likely a bunch more stuff needs to happen optionally
-    let _ = mount("proc", "/proc", "proc", MountFlags::empty(), None);
-    let _ = mount("sysfs", "/sys", "sysfs", MountFlags::empty(), None);
-    let _ = mkdir("/dev/pts", Mode::from_raw_mode(0o755));
-    let _ = mount("devpts", "/dev/pts", "devpts", MountFlags::empty(), None);
-}
 
 // kernel sigset_t size on x86-64 = _NSIG/8 = 8 bytes (NOT userspace sizeof)
 const SIGSET_SIZE: usize = 8;
@@ -35,8 +27,35 @@ const SIGTERM: u64 = 15;
 const WAIT_ANY: usize = usize::MAX; // pid = -1: reap any child
 const WNOHANG: usize = 1; // return immediately if none ready
 
-fn main() {
-    early_mounts();
+/// SignalSource is the tag we apply to our signalfd signals
+#[repr(u64)]
+enum SignalSource {
+    Signal = 1,
+    Stdin = 2,
+    Pty = 3,
+}
+
+impl SignalSource {
+    fn from_u64(v: u64) -> Option<Self> {
+        match v {
+            1 => Some(SignalSource::Signal),
+            2 => Some(SignalSource::Stdin),
+            3 => Some(SignalSource::Pty),
+            _ => None,
+        }
+    }
+}
+
+fn mount_system_paths() {
+    // TODO: this is sloppy af and needs a bunch of safeties
+    // and likely a bunch more stuff needs to happen optionally
+    let _ = mount("proc", "/proc", "proc", MountFlags::empty(), None);
+    let _ = mount("sysfs", "/sys", "sysfs", MountFlags::empty(), None);
+    let _ = mkdir("/dev/pts", Mode::from_raw_mode(0o755));
+    let _ = mount("devpts", "/dev/pts", "devpts", MountFlags::empty(), None);
+}
+
+fn connect_signalfd() -> anyhow::Result<OwnedFd> {
     // block SIGCHLD+SIGTERM, then signalfd them
     let mask: u64 = (1 << (SIGCHLD - 1)) | (1 << (SIGTERM - 1));
 
@@ -47,8 +66,7 @@ fn main() {
             &mask as *const u64 as usize, //set
             0usize,                       // oldset, unused
             SIGSET_SIZE
-        )
-        .unwrap();
+        )?;
     }
     let raw = unsafe {
         syscall!(
@@ -57,141 +75,179 @@ fn main() {
             &mask as *const u64 as usize,
             SIGSET_SIZE,
             SFD_CLOEXEC | SFD_NONBLOCK
-        )
-        .unwrap()
+        )?
     };
-    let sfd = unsafe { OwnedFd::from_raw_fd(raw as i32) };
+    unsafe { Ok(OwnedFd::from_raw_fd(raw as i32)) }
+}
 
+fn setup_pty() -> pty_process::Result<(OwnedFd, OwnedFd)> {
     // TODO : s/unwrap/doinitrite/g
-    let (mut pty, mut pts) = pty_process::blocking::open().unwrap();
-    let child = pty_process::blocking::Command::new("/bin/sh")
+    let (pty, pts) = pty_process::blocking::open()?;
+    let pty_fd: OwnedFd = rustix::io::dup(pty.as_fd())?;
+    let pts_fd: OwnedFd = rustix::io::dup(pts.as_fd())?;
+    pty_process::blocking::Command::new("/bin/sh")
         .spawn(pts)
         .unwrap();
+    Ok((pty_fd, pts_fd))
+}
 
-    let epfd = epoll::create(epoll::CreateFlags::CLOEXEC).unwrap();
-    #[repr(u64)]
-    enum Source {
-        Signal = 1,
-        Stdin = 2,
-        Pty = 3,
-    }
-
-    impl Source {
-        fn from_u64(v: u64) -> Option<Self> {
-            match v {
-                1 => Some(Source::Signal),
-                2 => Some(Source::Stdin),
-                3 => Some(Source::Pty),
-                _ => None,
-            }
+fn drain_signals(signal_fd: BorrowedFd) {
+    // drain signalfd
+    let mut buf = [0u8; 128 * 4];
+    while let Ok(n) = read(signal_fd, &mut buf) {
+        if n == 0 {
+            break;
         }
     }
+    // reap
+    loop {
+        match unsafe {
+            syscall!(
+                Sysno::wait4,
+                WAIT_ANY, // pid to listen for
+                0usize,   // don't capture exit status
+                WNOHANG,  // options
+                0usize    // don't capture resource usage
+            )
+        } {
+            Ok(0) | Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
+}
 
+fn forward_stdin(pty_fd: BorrowedFd) {
+    let mut buf = [0u8; 4096];
+    loop {
+        match rustix::io::read(stdin(), &mut buf) {
+            Ok(0) => break, // outside user EOF'd
+            Ok(n) => {
+                // n bytes to drain form stdin to master
+                let mut off = 0;
+                while off < n {
+                    match rustix::io::write(pty_fd, &buf[off..n]) {
+                        Ok(w) => off += w,
+                        Err(Errno::INTR) => continue,
+                        // TODO: can we lock up here?
+                        Err(Errno::AGAIN) => continue,
+                        Err(_e) => break, // master can't be written
+                    }
+                }
+            }
+            Err(Errno::AGAIN) => break, // stdin drained
+            Err(Errno::INTR) => continue,
+            Err(_e) => break,
+        }
+    }
+}
+
+enum PtyStatus {
+    Ok,
+    HungUp,
+}
+
+fn forward_pty(pty_fd: BorrowedFd) -> PtyStatus {
+    let mut stdout = std::io::stdout().lock();
+    let mut buf = [0u8; 4096];
+    loop {
+        match rustix::io::read(pty_fd, &mut buf) {
+            Ok(0) => return PtyStatus::HungUp,
+            Ok(n) => {
+                if stdout.write_all(&buf[..n]).is_err() {
+                    // yeah I know it's not ok
+                    // but probably this just means a temp write failure
+                    // like the writer refuses
+                    return PtyStatus::Ok;
+                };
+                let _ = stdout.flush();
+            }
+            Err(Errno::IO) => return PtyStatus::HungUp,
+            Err(Errno::AGAIN) => return PtyStatus::Ok, // spurious wakeup / drained; keep looping
+            Err(Errno::INTR) => continue,              // interrupted; retry next loop
+            Err(_e) => return PtyStatus::Ok,           // TODO: actually handle the error tho
+        };
+    }
+}
+
+fn setup_epoll(sfd: BorrowedFd, pty: BorrowedFd) -> io::Result<OwnedFd> {
+    let epfd = epoll::create(epoll::CreateFlags::CLOEXEC).unwrap();
     epoll::add(
         &epfd,
-        sfd.as_fd(),
-        epoll::EventData::new_u64(Source::Signal as u64),
+        sfd,
+        epoll::EventData::new_u64(SignalSource::Signal as u64),
         epoll::EventFlags::IN,
-    )
-    .unwrap();
+    )?;
     epoll::add(
         &epfd,
-        &stdin(),
-        epoll::EventData::new_u64(Source::Stdin as u64),
+        stdin(),
+        epoll::EventData::new_u64(SignalSource::Stdin as u64),
         epoll::EventFlags::IN,
-    )
-    .unwrap();
+    )?;
     epoll::add(
         &epfd,
-        &pty,
-        epoll::EventData::new_u64(Source::Pty as u64),
+        pty,
+        epoll::EventData::new_u64(SignalSource::Pty as u64),
         epoll::EventFlags::IN,
-    )
-    .unwrap();
+    )?;
+    Ok(epfd)
+}
 
-    let mut events: Vec<epoll::Event> = Vec::with_capacity(8);
+struct RawTerminal {
+    fd: BorrowedFd<'static>,
+    orig: rustix::termios::Termios,
+}
+impl RawTerminal {
+    fn make_raw(fd: BorrowedFd<'static>) -> anyhow::Result<Self> {
+        let orig = tcgetattr(fd)?;
+        let mut raw = orig.clone();
+        raw.make_raw();
+        tcsetattr(fd, OptionalActions::Flush, &raw)?;
+        Ok(RawTerminal { fd, orig })
+    }
 
-    // TODO: println can panic prefer writeln and eat errors
-    println!("spawned child with id {} ", child.id());
+    fn make_raw_stdin() -> anyhow::Result<Self> {
+        let stdin = unsafe { BorrowedFd::borrow_raw(0) };
+        RawTerminal::make_raw(stdin)
+    }
+
+    fn restore(&mut self) {
+        // TODO: how about making this stick and then non-op?
+        let _ = tcsetattr(self.fd, OptionalActions::Now, &self.orig);
+    }
+}
+impl Drop for RawTerminal {
+    fn drop(&mut self) {
+        self.restore();
+    }
+}
+
+fn main() {
+    let mut raw_terminal = RawTerminal::make_raw_stdin().unwrap();
+    mount_system_paths();
+    let sfd = connect_signalfd().unwrap();
+    let (pty_fd, _) = setup_pty().unwrap();
+    let epfd = setup_epoll(sfd.as_fd(), pty_fd.as_fd()).unwrap();
 
     // set non-blocking for the pty connections
-    fcntl_setfl(&pty, fcntl_getfl(&pty).unwrap() | OFlags::NONBLOCK).unwrap();
-    fcntl_setfl(&stdin(), fcntl_getfl(&stdin()).unwrap() | OFlags::NONBLOCK).unwrap();
+    fcntl_setfl(&pty_fd, fcntl_getfl(&pty_fd).unwrap() | OFlags::NONBLOCK).unwrap();
+    fcntl_setfl(stdin(), fcntl_getfl(stdin()).unwrap() | OFlags::NONBLOCK).unwrap();
 
+    let pty_fd = pty_fd.try_clone().unwrap();
+    let mut events: Vec<epoll::Event> = Vec::with_capacity(8);
     loop {
-        let mut stdout = std::io::stdout().lock();
         // clear
         events.clear();
         // collect anew
         epoll::wait(&epfd, spare_capacity(&mut events), None).unwrap();
         // consume
         for ev in events.iter() {
-            match Source::from_u64(ev.data.u64()) {
-                Some(Source::Signal) => {
-                    // drain signalfd
-                    let mut buf = [0u8; 128 * 4];
-                    while let Ok(n) = read(sfd.as_fd(), &mut buf) {
-                        if n == 0 {
-                            break;
-                        }
-                    }
-                    // reap
-                    loop {
-                        match unsafe {
-                            syscall!(
-                                Sysno::wait4,
-                                WAIT_ANY, // pid to listen for
-                                0usize,   // don't capture exit status
-                                WNOHANG,  // options
-                                0usize    // don't capture resource usage
-                            )
-                        } {
-                            Ok(0) | Err(_) => break,
-                            Ok(_) => continue,
-                        }
-                    }
-                }
-                Some(Source::Stdin) => {
-                    let mut buf = [0u8; 4096];
-                    loop {
-                        match rustix::io::read(stdin(), &mut buf) {
-                            Ok(0) => break, // outside user EOF'd
-                            Ok(n) => {
-                                // n bytes to drain form stdin to master
-                                let mut off = 0;
-                                while off < n {
-                                    match rustix::io::write(&pty, &buf[off..n]) {
-                                        Ok(w) => off += w,
-                                        Err(Errno::INTR) => continue,
-                                        // TODO: can we lock up here?
-                                        Err(Errno::AGAIN) => continue,
-                                        Err(_e) => break, // master can't be written
-                                    }
-                                }
-                            }
-                            Err(Errno::AGAIN) => break, // stdin drained
-                            Err(Errno::INTR) => continue,
-                            Err(_e) => break,
-                        }
-                    }
-                }
-                Some(Source::Pty) => {
-                    let mut buf = [0u8; 4096];
-                    loop {
-                        match rustix::io::read(&pty, &mut buf) {
-                            Ok(0) => { /* TODO: hungup, respawn shell and re-enter */ }
-                            Ok(n) => {
-                                if stdout.write_all(&buf[..n]).is_err() {
-                                    break;
-                                };
-                                let _ = stdout.flush();
-                            }
-                            Err(Errno::AGAIN) => break, // spurious wakeup / drained; keep looping
-                            Err(Errno::INTR) => continue, // interrupted; retry next loop
-                            Err(_e) => break,           // TODO: handle the error
-                        };
-                    }
-                }
+            match SignalSource::from_u64(ev.data.u64()) {
+                Some(SignalSource::Signal) => drain_signals(sfd.as_fd()),
+                Some(SignalSource::Stdin) => forward_stdin(pty_fd.as_fd()),
+                Some(SignalSource::Pty) => match forward_pty(pty_fd.as_fd()) {
+                    PtyStatus::Ok => {}
+                    PtyStatus::HungUp => raw_terminal.restore(),
+                },
                 None => {} // unknown tag; ignore
             }
         }
