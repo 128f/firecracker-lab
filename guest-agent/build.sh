dbf5a7debf4bcd447fc69301f0f122a110a37bb5
cburke <cburke91@pm.me>
2026-08-12 22:01:26 -0700

    Remove very old build.sh

--- guest-agent/build.sh
+++
@@ -1,7 +0,0 @@
-docker build . -t guest-agent:24.04
-CID=$(docker create guest-agent:24.04)
-docker export "$CID" -o rootfs.tar
-docker rm "$CID"
-
-mkdir -p rootfs-dir && tar -xf rootfs.tar -C rootfs-dir
-mkfs.ext4 -F -d rootfs-dir -b 4096 rootfs.ext4 2048M
